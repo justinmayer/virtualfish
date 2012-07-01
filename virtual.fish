3575e05380c82ee488ad1083f9f820cbac599639
Adam Brenecki <adam@brenecki.id.au>
2012-07-01 12:39:07 +0930

    Initial commit

---
+++ virtual.fish
@@ -0,0 +1,45 @@
+# VirtualFish
+# A Virtualenv wrapper for the Fish Shell based on Doug Hellman's virtualenvwrapper
+
+if not set -q VIRTUALENV_HOME
+	set -g VIRTUALENV_HOME $HOME/.virtualenvs
+end
+
+function avirtualenv --description "Activate a virtualenv"
+	# check arguments
+	if [ (count $argv) -lt 1 ]
+		echo "You need to specify a virtualenv."
+		return 1
+	end
+	if not [ -d $VIRTUALENV_HOME/$argv[1] ]
+		echo "The virtualenv $argv[1] does not exist."
+		echo "You can create it with mkvirtualenv."
+		return 2
+	end
+
+	#Check if a different env is being used
+	if set -q VIRTUAL_ENV
+		dvirtualenv
+	end
+
+	set -gx VIRTUAL_ENV $VIRTUALENV_HOME/$argv[1]
+	set -g _VF_EXTRA_PATH $VIRTUAL_ENV/bin
+	set -gx PATH $_VF_EXTRA_PATH $PATH
+end
+
+function dvirtualenv
+	#find elements to remove from PATH
+	set to_remove
+	for i in (seq (count $PATH))
+		if contains $PATH[$i] $_VF_EXTRA_PATH
+			set to_remove $to_remove $i
+		end
+	end
+
+	#remove them
+	for i in $to_remove
+		set -e PATH[$i]
+	end
+
+	set -e VIRTUAL_ENV
+end
