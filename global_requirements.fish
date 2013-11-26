c2103b6d430c86e3d1e93bce9f689ab2361e485f
Adam Brenecki <adam@brenecki.id.au>
2013-11-26 18:30:41 +1030

    Use wheel for global requirements if it's enabled.

--- global_requirements.fish
+++ global_requirements.fish
@@ -7,8 +7,14 @@ end
 function __vf_requirements --description "Edit the global requirements file for all virtualenvs"
     eval $EDITOR $VIRTUALFISH_HOME/global_requirements.txt
     pushd $VIRTUALFISH_HOME
-    for i in */bin/pip
-        eval $i install -r $VIRTUALFISH_HOME/global_requirements.txt
+    # If the user has set up wheels, make sure we build wheels of all the deps
+    # prior to trying to install them, so as to speed things up
+    if set -q PIP_USE_WHEEL
+        pip wheel -r $VIRTUALFISH_HOME/global_requirements.txt
     end
+    for i in */bin/pip
+        eval $i install -U -r $VIRTUALFISH_HOME/global_requirements.txt
+    end
+    popd
 end
 
