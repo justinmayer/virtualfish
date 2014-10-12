f68df871c53a21f96e5aa7a26ac5a1b86576afc3
Remco Wendt <remco.wendt@gmail.com>
2014-10-12 16:52:46 +0300

    Fixed file testing with empty string

--- environment_variables.fish
+++ environment_variables.fish
@@ -11,13 +11,13 @@ function __vf_set_environment_variables --on-event virtualenv_did_activate
     set -l venv_venvvar_file $VIRTUAL_ENV/$VIRTUALFISH_ENVIRONMENT_VARIABLES_FILE
 
     # Prefer project based environment config over virtualenv environment config
-    if [ -f $project_venvvar_file ]
+    if [ -f "$project_venvvar_file" ]
         set venvvar_file $project_venvvar_file
-    else if [ -f $venv_venvvar_file ]
+    else if [ -f "$venv_venvvar_file" ]
         set venvvar_file $venv_venvvar_file
     end
 
-    if [ -f $venvvar_file ]
+    if [ -f "$venvvar_file" ]
         cat $venvvar_file | while read -l key value
             # Save all currently existing env vars to their prefixed counter part
             if set -q $key
@@ -27,6 +27,7 @@ function __vf_set_environment_variables --on-event virtualenv_did_activate
             set -gx $key $value
         end
     end
+
 end
 
 function __vf_unset_environment_variables --on-event virtualenv_did_deactivate
@@ -35,13 +36,13 @@ function __vf_unset_environment_variables --on-event virtualenv_did_deactivate
     set -l venv_venvvar_file $VIRTUAL_ENV/$VIRTUALFISH_ENVIRONMENT_VARIABLES_FILE
 
     # Prefer project based environment config over virtualenv environment config
-    if [ -f $project_venvvar_file ]
+    if [ -f "$project_venvvar_file" ]
         set venvvar_file $project_venvvar_file
-    else if [ -f $venv_venvvar_file ]
+    else if [ -f  "$venv_venvvar_file" ]
         set venvvar_file $venv_venvvar_file
     end
 
-    if [ -f $venvvar_file ]
+    if [ -f "$venvvar_file" ]
         cat $venvvar_file | while read key value
             # Restore existing env vars
             set -l old_key $VIRTUALFISH_ENVIRONMENT_VARIABLES_PREFIX$key
