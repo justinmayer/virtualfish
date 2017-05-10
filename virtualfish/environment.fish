d18e966a31460ee142e071ac5c77b1ff81d25f09
Zach Sommers <github@zachsommers.com>
2017-05-10 10:09:57 -0700

    Convert env var value passing to echo
    Add comments for additional clarity
    Reorder some logic for legibility

--- virtualfish/environment.fish
+++ virtualfish/environment.fish
@@ -4,26 +4,28 @@ if not set -q VIRTUALFISH_ENVIRONMENT_FILE
 end
 
 function __vfsupport_set_env_file_path --description "Set VIRTUALFISH_ENVIRONMENT_FILE_PATH to appropriate value"
-    set -l vf_base (basename $VIRTUAL_ENV)
     set -l vf_path $VIRTUAL_ENV/$VIRTUALFISH_ENVIRONMENT_FILE
-    # Check if projects plugin is used
-    if set -q PROJECT_HOME
-        set -l project_path $PROJECT_HOME/$vf_base/$VIRTUALFISH_ENVIRONMENT_FILE
+    # Check if Projects plugin is used
+    if not set -q PROJECT_HOME
+        # Always look in the virtualenv dir when not using Projects plugin
+        echo $vf_path
+    else
+        set -l project_path $PROJECT_HOME/(basename $VIRTUAL_ENV)/$VIRTUALFISH_ENVIRONMENT_FILE
         if not test -r $project_path
             and test -r $vf_path
-            set -g VIRTUALFISH_ENVIRONMENT_FILE_PATH $vf_path
+            # Only use virtualenv dir when there is already a file there
+            echo $vf_path
         else
-            set -g VIRTUALFISH_ENVIRONMENT_FILE_PATH $project_path
+            # Prefer to use the project dir
+            echo $project_path
         end
-    else
-        set -g VIRTUALFISH_ENVIRONMENT_FILE_PATH $vf_path
     end
 end
 
 function __vfext_environment_activate --on-event virtualenv_did_activate
-    __vfsupport_set_env_file_path
+    set -l env_file_path (__vfsupport_set_env_file_path)
 
-    if test -r $VIRTUALFISH_ENVIRONMENT_FILE_PATH
+    if test -r $env_file_path
         while read -l line
             # Skip empty lines and comments
             if not string length -q $line
@@ -41,14 +43,14 @@ function __vfext_environment_activate --on-event virtualenv_did_activate
 
             # Eval to allow for expanding variables, e.g. PATH=$PATH foo
             set -gx $key (eval echo $value)
-        end < $VIRTUALFISH_ENVIRONMENT_FILE_PATH
+        end < $env_file_path
     end
 end
 
 function __vfext_environment_deactivate --on-event virtualenv_will_deactivate
-    __vfsupport_set_env_file_path
+    set -l env_file_path (__vfsupport_set_env_file_path)
 
-    if test -r $VIRTUALFISH_ENVIRONMENT_FILE_PATH
+    if test -r $env_file_path
         while read -l line
             # Skip empty lines and comments
             if not string length -q $line
@@ -66,18 +68,29 @@ function __vfext_environment_deactivate --on-event virtualenv_will_deactivate
             else
                 set -e $key
             end
-        end < $VIRTUALFISH_ENVIRONMENT_FILE_PATH
+        end < $env_file_path
     end
 end
 
 function __vf_environment --description "Edit the environment variables for the active virtual environment"
-    if set -q VIRTUAL_ENV
-        __vfsupport_set_env_file_path
-        # Deactivate before applying new env vars to avoid stomping stashed values
-        __vfext_environment_deactivate
-        eval $EDITOR $VIRTUALFISH_ENVIRONMENT_FILE_PATH
-        __vfext_environment_activate
-    else
+    # Requires active virtualenv
+    if not set -q VIRTUAL_ENV
         echo "Must have a virtual env active to run command"
+        return 1
     end
+
+    # Check if $VISUAL or $EDITOR is set, otherwise use vi (Git default)
+    if set -q VISUAL
+        set editor $VISUAL
+    else if set -q EDITOR
+        set editor $EDITOR
+    else
+        set editor vi
+    end
+
+    set -l env_file_path (__vfsupport_set_env_file_path)
+    # Deactivate before applying new env vars to avoid stomping stashed values
+    __vfext_environment_deactivate
+    eval $editor $env_file_path
+    __vfext_environment_activate
 end
