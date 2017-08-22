d2d4f127a797ad6e82d41de10edd0e70f1626df8
David Adam <zanchey@ucc.gu.uwa.edu.au>
2017-08-22 12:03:48 +0800

    Use 'source' command instead of deprecated '.' alias
    Closes #125

--- virtualfish/__main__.py
+++ virtualfish/__main__.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     commands = [
         'set -g VIRTUALFISH_VERSION {}'.format(version),
         'set -g VIRTUALFISH_PYTHON_EXEC {}'.format(sys.executable),
-        '. {}'.format(os.path.join(base_path, 'virtual.fish')),
+        'source {}'.format(os.path.join(base_path, 'virtual.fish')),
     ]
 
     for plugin in sys.argv[1:]:
         path = os.path.join(base_path, plugin + '.fish')
         if os.path.exists(path):
-            commands.append('. {}'.format(path))
+            commands.append('source {}'.format(path))
         else:
             print('virtualfish loader error: plugin {} does not exist!'.format(plugin), file=sys.stderr)
 
