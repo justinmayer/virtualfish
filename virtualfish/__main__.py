ba4f16f8eb37e2e60996b814a8423d4b9f1d65b6
Aleksandr Mezin <mezin.alexander@gmail.com>
2017-11-27 04:11:11 +0600

    Make virtualfish work without installation

--- virtualfish/__main__.py
+++ virtualfish/__main__.py
@@ -5,10 +5,14 @@ import pkg_resources
 
 
 if __name__ == "__main__":
-    version = pkg_resources.get_distribution('virtualfish').version
+    try:
+        version = pkg_resources.get_distribution('virtualfish').version
+        commands = ['set -g VIRTUALFISH_VERSION {}'.format(version)]
+    except pkg_resources.DistributionNotFound:
+        commands = []
+
     base_path = os.path.dirname(os.path.abspath(__file__))
-    commands = [
-        'set -g VIRTUALFISH_VERSION {}'.format(version),
+    commands += [
         'set -g VIRTUALFISH_PYTHON_EXEC {}'.format(sys.executable),
         'source {}'.format(os.path.join(base_path, 'virtual.fish')),
     ]
