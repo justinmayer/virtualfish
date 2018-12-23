c03db8997495c5ad9ada187d44938e9a10f1e59b
Abderrahim Kitouni <akitouni@gnome.org>
2018-12-23 10:48:53 +0100

    use only one cd in vf cdpackages
    Fix #95

--- virtualfish/virtual.fish
+++ virtualfish/virtual.fish
@@ -183,8 +183,11 @@ function __vf_cd --description "Change directory to this virtualenv"
 end
 
 function __vf_cdpackages --description "Change to the site-packages directory of this virtualenv"
-    vf cd
-    cd (find . -name site-packages -type d | head -n1)
+    if set -q VIRTUAL_ENV
+        cd (find $VIRTUAL_ENV -name site-packages -type d | head -n1)
+    else
+        echo "No virtualenv is active."
+    end
 end
 
 function __vf_tmp --description "Create a virtualenv that will be removed when deactivated"
