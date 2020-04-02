4284afb497dae4923a856785d17b4b6d145fc48a
Justin Mayer <entroP@gmail.com>
2020-04-02 08:24:15 +0200

    Add test for `vf addpath <path>`

---
+++ virtualfish/test/test_addpath.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def test_addpath(vf):
+    venv = f"{vf.homedir}/.virtualenvs/test"
+    vf.run("vf new test")
+    vf.run("vf activate test")
+    vf.run(f"mkdir {venv}/testpath")
+    vf.run(f"vf addpath {venv}/testpath")
+    out, _ = vf.run(f"{venv}/bin/python -c 'import sys; print(sys.path)'")
+    assert ".virtualenvs/test/testpath" in str(out)
