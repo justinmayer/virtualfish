39caca40117e5a05e483bf3386893eabe959ecfc
Justin Mayer <entroP@gmail.com>
2020-05-15 12:06:11 +0200

    Ensure minimum Fish shell version is installed
    Certain VirtualFish functionality depends on newer Fish shell features.
    Users on older Fish versions who don't notice the minimum version
    requirement will subsequently encounter errors. This adds a function to
    the initial Python-based installation process that exits and notifies if
    the minimum Fish shell version requirement is not met.

--- virtualfish/loader/cli.py
+++ virtualfish/loader/cli.py
@@ -4,6 +4,9 @@ import psutil
 from virtualfish.loader import installer
 
 
+minimum_fish_version = "3.1"
+
+
 def install():
     if "--help" in argv or "-h" in argv:
         print("Usage: vf install [<plugin> ...]")
@@ -11,7 +14,25 @@ def install():
     installer.install(argv[2:])
 
 
+def check_fish_version():
+    """Exit and notify if minimum Fish version is not installed. Bail silently if
+       'packaging' module is missing or if Fish is not installed."""
+    try:
+        import subprocess
+        from packaging import version
+
+        cmd = ["fish", "-c", "echo $version"]
+        fish_version = subprocess.check_output(cmd).decode("utf-8").strip()
+        if version.parse(fish_version) < version.parse(minimum_fish_version):
+            print("VirtualFish requires Fish {} or higher. Current version: {}".format(
+                minimum_fish_version, fish_version))
+            exit()
+    except (ModuleNotFoundError, FileNotFoundError):
+        pass
+
+
 def main():
+    check_fish_version()
     if len(argv) >= 2 and argv[1] == "install":
         install()
         print(
@@ -31,7 +52,8 @@ def main():
                 "load it."
             )
         else:
-            print("VirtualFish isn't compatible with {}, only Fish.".format(the_shell))
+            print("VirtualFish isn't compatible with {}, only Fish {}+.".format(
+                the_shell, minimum_fish_version))
 
 
 if __name__ == "__main__":
