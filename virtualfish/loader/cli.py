dca90659ea78fb337189d91b8b46fa323c3c186e
Justin Mayer <entroP@gmail.com>
2020-05-25 11:48:03 +0200

    Improve Fish version check; don't exit on failure
    Fish's version number in its master branch has a hyphen-suffixed
    addendum that causes incorrect version comparison results, so now that
    suffix is stripped before comparing to the minimum version number.
    
    If that check fails, VirtualFish now shows a warning and continues
    instead of exiting with an error, in an effort to more gracefully handle
    situations where the version check fails even though the minimum Fish
    version requirement is met. This may cause problems if the check results
    are actually correct and folks ignore the warning, since subsequent
    VirtualFish invocations such as `vf new` may yield expected results on
    older versions of Fish.
    
    Fixes #177

--- virtualfish/loader/cli.py
+++ virtualfish/loader/cli.py
@@ -1,10 +1,17 @@
 from sys import argv
+import logging
 import psutil
 
 from virtualfish.loader import installer
 
 
-minimum_fish_version = "3.1"
+logger = logging.getLogger(__name__)
+minimum_fish_version = "3.3.0"
+
+
+class vcolors:
+    NORMAL = "\033[0m"
+    RED = "\033[31m"
 
 
 def install():
@@ -15,18 +22,20 @@ def install():
 
 
 def check_fish_version():
-    """Exit and notify if minimum Fish version is not installed. Bail silently if
-       'packaging' module is missing or if Fish is not installed."""
+    """Display a warning if the minimum Fish version is not installed. Bail silently if
+       the 'packaging' module is missing or if Fish is not installed."""
     try:
         import subprocess
         from packaging import version
 
         cmd = ["fish", "-c", "echo $version"]
         fish_version = subprocess.check_output(cmd).decode("utf-8").strip()
+        # Remove any extraneous hyphen-suffixed bits
+        fish_version = fish_version.partition("-")[0]
         if version.parse(fish_version) < version.parse(minimum_fish_version):
-            print("VirtualFish requires Fish {} or higher. Current version: {}".format(
-                minimum_fish_version, fish_version))
-            exit()
+            logger.warning(
+                "{}WARNING: VirtualFish requires Fish {} or higher. Current version: {}{}".format(
+                vcolors.RED, minimum_fish_version, fish_version, vcolors.NORMAL))
     except (ModuleNotFoundError, FileNotFoundError):
         pass
 
@@ -36,8 +45,8 @@ def main():
     if len(argv) >= 2 and argv[1] == "install":
         install()
         print(
-            "VirtualFish is now installed! Run 'exec fish' to reload "
-            "Fish, and you'll be able to use it!"
+            "VirtualFish is now installed! To enable it for this session, "
+            "run 'exec fish' to reload Fish."
         )
     else:
         # Display an error prompting the user to run `vf install`, if they are
