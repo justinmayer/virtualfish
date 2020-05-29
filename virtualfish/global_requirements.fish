6ea126724e0f794e3fe9e22ee70efaba910aa1d6
Justin Mayer <entroP@gmail.com>
2020-05-29 14:45:53 +0200

    global_requirements: Don't manually build wheels
    PIP_USE_WHEEL was removed in Pip 10.0, and there is no need to build
    wheels automatically since Pip should cache wheels by default.

--- virtualfish/global_requirements.fish
+++ virtualfish/global_requirements.fish
@@ -1,18 +1,15 @@
-function __vfext_global_requirements --on-event virtualenv_did_create
+function __vfext_global_requirements --on-event virtualenv_did_create --description "Install global requirements when new virtual environment is created"
     if begin; test -f $VIRTUALFISH_HOME/global_requirements.txt; and test "$VIRTUALFISH_GLOBAL_REQUIREMENTS" != "0"; end
+        echo "Installing global requirements..."
         pip install -U -r $VIRTUALFISH_HOME/global_requirements.txt
     end
 end
 
-function __vf_requirements --description "Edit the global requirements file for all virtualenvs"
+function __vf_requirements --description "Edit the global requirements file and install contents into *all* virtual environments"
     eval $EDITOR $VIRTUALFISH_HOME/global_requirements.txt
     pushd $VIRTUALFISH_HOME
-    # If the user has set up wheels, make sure we build wheels of all the deps
-    # prior to trying to install them, so as to speed things up
-    if set -q PIP_USE_WHEEL
-        pip wheel -r $VIRTUALFISH_HOME/global_requirements.txt
-    end
     for i in */bin/pip
+        echo "Installing global requirements via" $i "..."
         eval $i install -U -r $VIRTUALFISH_HOME/global_requirements.txt
     end
     popd
