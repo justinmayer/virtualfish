12489c4d977154d92aebaecbf65ae87a228a5653
Justin Mayer <entroP@gmail.com>
2020-09-02 13:46:56 +0200

    Add function to check if given Python works properly
    Given a path to a Python interpreter, run `python -V` to ensure it
    isn't broken. Adding the --pip flag will run `python -m pip -V` to also
    ensure that Pip functions correctly.

--- virtualfish/virtual.fish
+++ virtualfish/virtual.fish
@@ -582,3 +582,18 @@ function __vf_rmplugins --description "Remove one or more plugins"
     set -l python (__vfsupport_get_default_python --exec)
     $python -m virtualfish.loader.installer rmplugins $argv
 end
+
+function __vfsupport_check_python --description "Ensure Python/Pip are in a working state"
+    argparse "p/pip" -- $argv
+    set -l python_path $argv[1]
+    set -l pipflag ""
+    if set -q _flag_pip
+        set pipflag "-m pip"
+    end
+    set -l test_py (fish -c "'$python_path' $pipflag -V" 2>/dev/null)
+    if test $status -ne 0
+        return 1
+    else
+        return 0
+    end
+end
